027b90d91fa9307f067804148ec1d14a296305b3
Andhi Noerdianto <blog@cahkaibon-laptop.(none)>
2013-03-06 22:36:21 +0700

    first post

--- config.rb
+++ config.rb
@@ -2,10 +2,10 @@
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/cahkaibon/"
-http_images_path = "/cahkaibon/images"
-http_fonts_path = "/cahkaibon/fonts"
-css_dir = "public/cahkaibon/stylesheets"
+http_path = "/"
+http_images_path = "/images"
+http_fonts_path = "/fonts"
+css_dir = "public/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
---
+++ source/_posts/2013-03-06-hello-world.markdown
@@ -0,0 +1,7 @@
+---
+layout: post
+title: "hello world"
+date: 2013-03-06 22:29
+comments: true
+categories: 
+---
